c747bdc765abf194608fe97bad0aa941872e34b2
Steven Clontz <steven.clontz@gmail.com>
2022-12-20 13:49:26 -0600

    Initial commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo "Installing CodeChat"
+# pip install CodeChat-Server
+
+echo "Install LaTeX"
+# apt-get update && apt-get install -y texlive texlive-latex-extra texlive-fonts-extra texlive-xetex texlive-science --no-install-recommends
+
